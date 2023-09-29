32950893bb905b23af962948d276903e971fa34f
Yael Orellano <yael.e.orellano@gmail.com>
2023-09-29 15:24:55 -0300

    se añadio pagina principal ndel sitio

---
+++ router.php
@@ -0,0 +1,21 @@
+<?php
+require_once './app/controllers/HomeController.php';
+
+define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+
+$action = 'home'; // accion por defecto
+if (!empty( $_GET['action'])) {
+    $action = $_GET['action'];
+}
+
+$params = explode('/', $action);
+
+switch ($params[0]) {
+    case 'home':
+        $controller = new HomeController();
+        $controller->showHome();
+        break;
+    default: 
+        echo "404 Page Not Found";
+        break;
+}
---
+++ templates/footer.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="css/footer.css">
+</div>
+</main>
+
+
+
+<footer>
+      <p>Contacto: mail1@gmail.com </p>
+
+<p>No dudes en ponerte en contacto
+   con nosotros para comenzar a 
+   planificar tu próxima aventura. 
+   ¡Estamos ansiosos por ser parte de tus recuerdos de viaje inolvidables!</p>
+</footer>
+    
+
+</body>
+</html>
