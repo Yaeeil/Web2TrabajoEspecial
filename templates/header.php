a18e5509c8f2cb0e6f9ee0fc8816319bb3b5ed61
Yael Orellano <yael.e.orellano@gmail.com>
2023-09-29 15:24:55 -0300

    se añadio carpetas principales y pagina de inicio

---
+++ templates/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <base href="<?php echo BASE_URL ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Viajes</title>
+</head>
+
+<body>
+    <header>
+        
+    <div class="barraNav">
+    <h1>Travel Around The World</h1>
+            <a href="<?php echo BASE_URL . 'home'; ?>">Home</a>
+            <a href="<?php echo BASE_URL . 'clientes'; ?>">Clientes</a>
+            <a href="<?php echo BASE_URL . 'viajes'; ?>">Viajes</a>
+            <a href="<?php echo BASE_URL . 'logIn'; ?>">Ingresar</a>
+    </div>
+</header>
+<div class="container">
