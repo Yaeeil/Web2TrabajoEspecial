e846c1e7e27534ef6035fe67c9134e4328eb8b12
Joaquin Corbellini <Corbellini47@gmail.com>
2023-10-10 22:04:15 -0300

    Mover bootstrap y script a header

--- router.php
+++ router.php
@@ -1,5 +1,6 @@
 <?php
 define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
+
 require_once 'templates/header.phtml';
 
 require_once './app/controllers/HomeController.php';
